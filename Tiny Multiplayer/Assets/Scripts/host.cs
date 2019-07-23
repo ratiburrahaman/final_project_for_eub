9ce7cd1c6d4a123ed88d6be2678168a60ecbbcbc
Ratibur Rahaman <33982671+ratiburrahaman@users.noreply.github.com>
2019-07-23 16:25:58 +0600

    Update

---
+++ Tiny Multiplayer/Assets/Scripts/host.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.Networking.Match;
+
+public class host : MonoBehaviour
+{
+    NetworkManager networkManager;
+    public InputField roomNamefield;
+    public string roomName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if (networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void createHost()
+    {
+        roomName = roomNamefield.text;
+
+        roomName = roomNamefield.text;
+
+        networkManager.matchMaker.CreateMatch(roomName, 4, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+    }
+
+}
